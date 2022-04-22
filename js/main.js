ed74bc22731433ca173df0264b424e0ea98fb2a3
miss-gif <ddc.akari@gmail.com>
2022-04-22 16:52:06 +0900

    JS 작업

--- js/main.js
+++ js/main.js
@@ -56,21 +56,16 @@ navList[5].addEventListener("click", function () {
   navList[5].classList.add("onClick");
 });
 
-// new Swiper(".UNIVERSE .swiper-container", {
-//   // direction: 'horizontal', // 수평 슬라이드
-//   // autoplay: true, // 자동 재생 여부
-//   loop: true, // 반복 재생 여부
-//   spaceBetween: 30, // 슬라이드 사이 여백
-//   slidesPerView: 5, // 한 번에 보여줄 슬라이드 개수
-//   slidesPerGroup: 1, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
-//   pagination: {
-//     // 페이지 번호 사용 여부
-//     el: ".about .swiper-pagination", // 페이지 번호 요소 선택자
-//     clickable: true, // 사용자의 페이지 번호 요소 제어 가능 여부
-//   },
-//   navigation: {
-//     // 슬라이드 이전/다음 버튼 사용 여부
-//     prevEl: ".about .swiper-prev", // 이전 버튼 선택자
-//     nextEl: ".about .swiper-next", // 다음 버튼 선택자
-//   },
-// });
+// 슬라이드 설정
+
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal", // 수평 슬라이드
+  // autoplay: {
+  //   // 자동 재생 여부
+  //   // delay: 5000, // 5초마다 슬라이드 바뀜
+  // },
+  loop: true, // 반복 재생 여부
+  spaceBetween: 20, // 슬라이드 사이 여백
+  slidesPerView: 5, // 한 번에 보여줄 슬라이드 개수
+  slidesPerGroup: 1, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
+});
